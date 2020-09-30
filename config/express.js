323e6718e61ca626e220fe7bd5ec426acd94fed0
dplamenov <dimitar.plamenov@gmail.com>
2020-09-30 19:47:44 +0300

    update project structure

--- config/express.js
+++ config/express.js
@@ -4,7 +4,6 @@ module.exports = function () {
     const _render = response.render;
 
     response.render = function (viewName, data) {
-        console.log(this, viewName, { ...data, name: 'demo', layout: false });
         _render.call(this, viewName, { ...data, name: 'demo', layout: false });
     }
 }
