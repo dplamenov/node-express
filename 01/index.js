df278541eb12023af788ccc498a274cc21b861a8
dplamenov <dimitar.plamenov@gmail.com>
2020-09-30 19:41:20 +0300

    install express-handlebars

--- 01/index.js
+++ 01/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const hbs = require('express-handlebars');
 
 const router = require('./router');
 
 const app = express();
+app.set('view engine', '.hbs');
+app.engine('.hbs', hbs({ extname: '.hbs' }));
+
 app.use(router);
 
 
