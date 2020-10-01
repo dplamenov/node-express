2dcbf18e5e05c8738899a3839f6fd1fba8bfba3d
dplamenov <dimitar.plamenov@gmail.com>
2020-10-01 09:29:08 +0300

    use express-validator to validate form

--- router.js
+++ router.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+const { body, validationResult } = require('express-validator');
+
 router.get('/', (req, res) => {
     res.render('home');
 });
 
-router.post('/', (req, res) => {
+router.post('/', [
+    body('data').isLength({ min: 3 }).withMessage('data must be at least 3 chars long'),
+], (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.render('home', { errors: errors.array() });
+    }
+
     const { data } = req.body;
     console.log(data);
 
-    res.end();
+    res.redirect('/');
 });
 
 module.exports = router;
